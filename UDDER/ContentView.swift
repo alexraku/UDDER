0ddb325b899b81c65d2ec0a66bb48d7fc7364d10
Alex <raku@crtx.ru>
2021-08-04 16:08:00 +0700

    name and description has added

--- UDDER/ContentView.swift
+++ UDDER/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("udder".uppercased())
+                .font(.title)
+            Text("fresh milk at demand")
+                .font(.subheadline)
+            Spacer()
+        }
+        
     }
 }
 
